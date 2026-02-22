e650656bb3824d57788e724bbd5a827dc56c4cc4
John Gonon <pantolomin@hotmail.com>
2026-02-22 09:58:14 +0100

    start chapter 3 + correct chapter 2

--- chapter_02/src/main.rs
+++ chapter_02/src/main.rs
@@ -1,10 +1,10 @@
-use rand::Rng;
+use rand::RngExt;
 use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::rng().random_range(1..=100);
 
     let mut iteration = 1;
     loop {
